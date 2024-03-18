dbd3c8f41a8ce51178e250e81b7424dabc072e11
Kishore Koppula <55914125+kishore906@users.noreply.github.com>
2024-03-18 16:42:38 +1100

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,2 @@
-export const PORT = 3001;
-export const mongoDBURL =
-  "mongodb+srv://<username>:<password>@mern-crud-app.vuzopqp.mongodb.net/bookstore-crud-mern?retryWrites=true&w=majority&appName=MERN-CRUD-app";
+export const PORT=3001;
+export const mongoDBURL=""
